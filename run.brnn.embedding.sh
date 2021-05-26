ae0413faacd74a583b7297c74e361b2f100d2ae1
tiago.blima <tiago.blima02@gmail.com>
2021-05-26 15:18:58 -0300

    curating files

--- run.brnn.embedding.sh
+++ run.brnn.embedding.sh
@@ -5,6 +5,6 @@ pip install numpy==1.20.0
 cd ts-corpus-mt/ || exit
 mkdir "glove_dir"
 wget glove_s300.zip http://143.107.183.175:22980/download.php?file=embeddings/glove/glove_s300.zip
-unzip glove_s300.zip -d "glove_dir"
-python experiment_setup.py --model brnn --embedding
+unzip -O glove_s300.zip -d "glove_dir"
+python -O experiment_setup.py --model brnn --embedding
 #python experiment_setup.py --model transformer
