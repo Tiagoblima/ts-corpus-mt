315cbcd228af154f10509ac3cb5e188508da7671
tiago.blima <tiago.blima02@gmail.com>
2021-05-25 21:39:14 -0300

    passing model as args

--- experiment_setup.py
+++ experiment_setup.py
@@ -54,7 +54,7 @@ def create_config_file(folder_name_):
         file.write('\ngpu_ranks: [0]\n')
         file.write("batch_size: 4\nvalid_batch_size: 4")
     else:
-        training_steps = 1000
+        training_steps = 3000
         file.write(f"\nsave_checkpoint_steps: {training_steps}\ntrain_steps: {training_steps}")
         file.write("\nbatch_size: 32\nvalid_batch_size: 32")
     file.close()
@@ -66,7 +66,15 @@ for folder_name in os.listdir('datasets/'):
     target = folder_name.split('-')[1]
     try:
         os.makedirs(os.path.join(ENCODER, "reports"))
+    except OSError:
+        pass
+
+    try:
         os.makedirs(os.path.join(ENCODER, "prediction"))
+    except OSError:
+        pass
+
+    try:
         os.makedirs(os.path.join(ENCODER, "config_files"))
     except OSError:
         pass
