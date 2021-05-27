78c4d3e4eff8b4879c599388c117b356731468b0
tiago.blima <tiago.blima02@gmail.com>
2021-05-27 10:54:45 -0300

    curating files

--- run.transformer.embedding.sh
+++ run.transformer.embedding.sh
@@ -3,6 +3,13 @@ pip install nltk
 pip install openNMT-py
 pip install numpy==1.20.0
 cd ts-corpus-mt/ || exit
-wget -O glove_s300.zip http://143.107.183.175:22980/download.php?file=embeddings/glove/glove_s300.zip
-unzip glove_s300.zip -d "glove_dir"
-python experiment_setup.py --model transformer --embedding
+if [ -d "glove_dir" ]
+then
+     echo "Glove dir exists"
+else
+    mkdir "glove_dir"
+    wget -O glove_s300.zip http://143.107.183.175:22980/download.php?file=embeddings/glove/glove_s300.zip
+    unzip glove_s300.zip -d "glove_dir"
+fi
+
+python -O experiment_setup.py --model brnn --embedding
