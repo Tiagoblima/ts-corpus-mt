357575d2a145adc44b01bcacb1b99735e383738d
tiago.blima <tiago.blima02@gmail.com>
2021-05-28 09:27:49 -0300

    curating files

--- experiment_setup.py
+++ experiment_setup.py
@@ -60,11 +60,11 @@ def create_config_file(folder_name_):
 
         file.write(f"\nsave_checkpoint_steps: {training_steps}\ntrain_steps: {training_steps}")
         file.write('\ngpu_ranks: [0]\n')
-        file.write("batch_size: 32\nvalid_batch_size: 32")
+        file.write("batch_size: 64\nvalid_batch_size: 64")
     else:
 
         file.write(f"\nsave_checkpoint_steps: {training_steps}\ntrain_steps: {training_steps}")
-        file.write("\nbatch_size: 32\nvalid_batch_size: 32")
+        file.write("\nbatch_size: 64\nvalid_batch_size: 64")
     file.close()
     return config_file_path
 
@@ -92,10 +92,12 @@ for folder_name in os.listdir('datasets/'):
     os.system(f'onmt_build_vocab -config {config_path} -n_sample 10000')
     os.system(f'onmt_train -config {config_path}')
     test_file = f"datasets/{folder_name}/test.{source}"
+
     if args.embedding:
         pred_file = f"{ENCODER}/prediction/{source}-{target}.embedding-pred.txt"
     else:
         pred_file = f"{ENCODER}/prediction/{source}-{target}-pred.txt"
+
     translate_cmd = f'onmt_translate -model {ENCODER}/run/{folder_name}/model_step_{training_steps}.pt -src {test_file} -output {pred_file} -verbose '
     if torch.cuda.is_available():
         translate_cmd += ' -gpu 0'
